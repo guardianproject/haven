11953e70b1a6d2f88908340d812937094a0f9df8
n8fr8 <nathan@freitas.net>
2017-04-17 00:48:39 -0400

    more progress on #15 for the alert/event log

--- src/main/java/info/guardianproject/phoneypot/ui/EventActivity.java
+++ src/main/java/info/guardianproject/phoneypot/ui/EventActivity.java
@@ -37,7 +37,25 @@ public class EventActivity extends AppCompatActivity {
             {
 
                 mEventLog.append("Event Triggered @ " + eventTrigger.getTriggerTime().toLocaleString()).append("\n");
-                mEventLog.append("Event Type: " + eventTrigger.getType());
+
+                String sType = "";
+
+                switch (eventTrigger.getType())
+                {
+                    case EventTrigger.ACCELEROMETER:
+                        sType = "ACCELEROMETER";
+                        break;
+                    case EventTrigger.CAMERA:
+                        sType = "CAMERA MOTION";
+                        break;
+                    case EventTrigger.MICROPHONE:
+                        sType = "SOUND";
+                        break;
+                    default:
+                        sType = "UNKNOWN";
+                }
+
+                mEventLog.append("Event Type: " + sType);
                 mEventLog.append("\n==========================\n");
             }
 
