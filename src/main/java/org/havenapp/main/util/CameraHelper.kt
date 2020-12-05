a90c92ae1370784cdee5e5261ef94e2b6be51fd2
Arka Prava Basu <arkaprava94@gmail.com>
2020-12-05 12:08:30 +0530

    Add more helpers
    Signed-off-by: Arka Prava Basu <arkaprava94@gmail.com>

--- src/main/java/org/havenapp/main/util/CameraHelper.kt
+++ src/main/java/org/havenapp/main/util/CameraHelper.kt
@@ -2,10 +2,8 @@ package org.havenapp.main.util
 
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraMetadata
-
-fun CameraCharacteristics.isLegacyDevice(): Boolean =
-        (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
-                CameraMetadata.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY)
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 fun CameraCharacteristics.isLimitedLevelDevice(): Boolean =
         (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
@@ -15,3 +13,16 @@ fun CameraCharacteristics.isFullLevel(): Boolean =
         (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
                 CameraMetadata.INFO_SUPPORTED_HARDWARE_LEVEL_FULL)
 
+fun CameraCharacteristics.isLegacyDevice(): Boolean =
+        (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
+                CameraMetadata.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY)
+
+@RequiresApi(Build.VERSION_CODES.N)
+fun CameraCharacteristics.isLevel3(): Boolean =
+        (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
+                CameraMetadata.INFO_SUPPORTED_HARDWARE_LEVEL_3)
+
+@RequiresApi(Build.VERSION_CODES.P)
+fun CameraCharacteristics.isLevelExternal(): Boolean =
+        (this[CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL] ==
+                CameraMetadata.INFO_SUPPORTED_HARDWARE_LEVEL_EXTERNAL)
