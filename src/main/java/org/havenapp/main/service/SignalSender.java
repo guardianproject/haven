e57cb9017c172f384afa0717b528209358075a88
n8fr8 <nathan@guardianproject.info>
2021-03-19 10:41:10 -0400

    move from signal-cli to libsignal directly

--- src/main/java/org/havenapp/main/service/SignalSender.java
+++ src/main/java/org/havenapp/main/service/SignalSender.java
@@ -1,25 +1,47 @@
 package org.havenapp.main.service;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.CountDownTimer;
-import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.util.Log;
 
-import net.sourceforge.argparse4j.inf.Namespace;
+import androidx.annotation.Nullable;
 
 import org.asamk.signal.Main;
 import org.havenapp.main.PreferenceManager;
 import org.havenapp.main.R;
 import org.havenapp.main.Utils;
+import org.whispersystems.libsignal.IdentityKey;
+import org.whispersystems.libsignal.IdentityKeyPair;
+import org.whispersystems.libsignal.InvalidKeyException;
+import org.whispersystems.libsignal.InvalidKeyIdException;
+import org.whispersystems.libsignal.SignalProtocolAddress;
+import org.whispersystems.libsignal.state.PreKeyRecord;
+import org.whispersystems.libsignal.state.SessionRecord;
+import org.whispersystems.libsignal.state.SignalProtocolStore;
+import org.whispersystems.libsignal.state.SignedPreKeyRecord;
+import org.whispersystems.libsignal.util.KeyHelper;
+import org.whispersystems.libsignal.util.guava.Optional;
+import org.whispersystems.signalservice.api.SignalServiceAccountManager;
+import org.whispersystems.signalservice.api.SignalServiceMessagePipe;
+import org.whispersystems.signalservice.api.SignalServiceMessageSender;
+import org.whispersystems.signalservice.api.crypto.UntrustedIdentityException;
+import org.whispersystems.signalservice.api.messages.SignalServiceAttachment;
+import org.whispersystems.signalservice.api.messages.SignalServiceDataMessage;
+import org.whispersystems.signalservice.api.push.SignalServiceAddress;
+import org.whispersystems.signalservice.api.push.TrustStore;
+import org.whispersystems.signalservice.internal.configuration.SignalCdnUrl;
+import org.whispersystems.signalservice.internal.configuration.SignalServiceConfiguration;
+import org.whispersystems.signalservice.internal.configuration.SignalServiceUrl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by n8fr8 on 11/6/17.
@@ -30,6 +52,7 @@ public class SignalSender {
     private Context mContext;
     private static SignalSender mInstance;
     private String mUsername; //aka your signal phone number
+    private String mPassword;
     private CountDownTimer mCountdownTimer;
     private PreferenceManager preferences;
     private String messageString;
@@ -38,31 +61,40 @@ public class SignalSender {
     private int interval;
     private int mAlertCount;
 
-    private SignalSender(Context context, String username)
-    {
+    private final String     URL         = "https://my.signal.server.com";
+    private final SignalProtocolStore PROTOCOL_STORE = new HavenSignalProtocolStore();
+    private final String     USER_AGENT  = "(Haven/Android)";
+
+    private SignalServiceAccountManager mManager;
+    private SignalServiceConfiguration mService;
+
+    private SignalSender(Context context) throws FileNotFoundException {
         mContext = context;
-        mUsername = username;
         mAlertCount = 0;
         preferences = new PreferenceManager(mContext);
         prefix = preferences.getHeartbeatPrefix();
         suffix = preferences.getHeartbeatSuffix();
         messageString = preferences.getHeartbeatMonitorMessage();
         interval = preferences.getHeartbeatNotificationTimeMs() / 60000;
+
+        SignalServiceUrl[] signalServiceUrls = {};
+        SignalCdnUrl[] signalCdnUrls = {};
+        mService = new SignalServiceConfiguration(signalServiceUrls,signalCdnUrls);
     }
 
-    public static synchronized SignalSender getInstance (Context context, String username)
-    {
+    public static synchronized SignalSender getInstance (Context context) throws FileNotFoundException {
         if (mInstance == null)
         {
-            mInstance = new SignalSender(context, username);
+            mInstance = new SignalSender(context);
         }
 
         return mInstance;
     }
 
-    public void setUsername (String username)
+    public void setCredentials (String username, String password)
     {
         mUsername = username;
+        mPassword = password;
     }
 
     public void reset ()
@@ -72,25 +104,40 @@ public class SignalSender {
         mInstance = null;
     }
 
-    public void register (boolean callEnabled, @Nullable SignalExecutorTask.TaskResult taskResult) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("username", mUsername);
-        map.put("command", "register");
-        if (!callEnabled)
-            map.put("voice", false);
-        else
-            map.put("voice", true);
+    public void register (boolean callEnabled, @Nullable SignalExecutorTask.TaskResult taskResult) throws IOException {
 
-        execute(map, taskResult);
+        int deviceId = 9999;
+        String userAgent = "(Haven/Android)";
+
+        mManager = new SignalServiceAccountManager(mService,mUsername,mPassword,deviceId,userAgent);
+        mManager.requestSmsVerificationCode();
+
     }
 
-    public void verify (final String verificationCode, @Nullable SignalExecutorTask.TaskResult taskResult) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("username", mUsername);
-        map.put("command", "verify");
-        map.put("verificationCode", verificationCode);
+    public void verify (final String receivedSmsVerificationCode, @Nullable SignalExecutorTask.TaskResult taskResult) throws IOException, InvalidKeyException {
 
-        execute(map, taskResult);
+        int signedPreKeyId = 1;
+        String pinCode = "9999";
+
+        mManager.verifyAccountWithCode(receivedSmsVerificationCode, generateRandomSignalingKey(),
+                generateRandomInstallId(), false, pinCode);
+
+        IdentityKeyPair identityKey        = KeyHelper.generateIdentityKeyPair();
+        List<PreKeyRecord> oneTimePreKeys     = KeyHelper.generatePreKeys(0, 100);
+        SignedPreKeyRecord signedPreKeyRecord = KeyHelper.generateSignedPreKey(identityKey, signedPreKeyId);
+
+        mManager.setPreKeys(identityKey.getPublicKey(), signedPreKeyRecord, oneTimePreKeys);
+
+    }
+
+    private String generateRandomSignalingKey ()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+    private int generateRandomInstallId ()
+    {
+        return (int)(Math.random()*100000);
     }
 
     public void stopHeartbeatTimer ()
@@ -135,14 +182,19 @@ public class SignalSender {
         else
             messageString = emojiString + "\n" + mContext.getString(R.string.battery_level_msg_text) + ": " + Utils.getBatteryPercentage(mContext) + "%";
 
-        initHbMessage(messageString);
+        try {
+            initHbMessage(messageString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UntrustedIdentityException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initHbMessage (String message)
-    {
+    private void initHbMessage (String message) throws IOException, UntrustedIdentityException {
         if (!TextUtils.isEmpty(mUsername)) {
             mAlertCount ++;
-            getInstance(mContext, mUsername.trim());
+            getInstance(mContext);
             ArrayList<String> recipient = new ArrayList<>();
             recipient.add(preferences.getRemotePhoneNumber());
             sendMessage(recipient, message,null, null);
@@ -150,28 +202,138 @@ public class SignalSender {
     }
 
     public void sendMessage (final ArrayList<String> recipients, final String message,
-                             final String attachment, @Nullable SignalExecutorTask.TaskResult listener) {
+                             final String attachmentPath, @Nullable SignalExecutorTask.TaskResult listener) throws IOException, UntrustedIdentityException {
 
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("username", mUsername);
-        map.put("endsession",false);
-        map.put("recipient", recipients);
-        map.put("command", "send");
-        map.put("message", message);
 
-        if (attachment != null) {
-            ArrayList<String> attachments = new ArrayList<>();
-            attachments.add(attachment);
-            map.put("attachment",attachments);
+        SignalServiceMessageSender messageSender =
+                new SignalServiceMessageSender(mService, mUsername, mPassword, PROTOCOL_STORE, USER_AGENT, Optional.absent(), Optional.absent());
+
+        if (attachmentPath != null) {
+
+            File myAttachment     = new File(attachmentPath);
+            FileInputStream attachmentStream = new FileInputStream(myAttachment);
+            SignalServiceAttachment attachment       = SignalServiceAttachment.newStreamBuilder()
+                    .withStream(attachmentStream)
+                    .withContentType("image/jpeg")
+                    .withLength(myAttachment.length())
+                    .build();
+
+            for (String recipient : recipients) {
+                messageSender.sendMessage(new SignalServiceAddress(recipient),
+                        SignalServiceDataMessage.newBuilder()
+                                .withBody(message)
+                                .withAttachment(attachment)
+                                .build());
+            }
+        }
+        else
+        {
+            for (String recipient: recipients) {
+                messageSender.sendMessage(new SignalServiceAddress(recipient),
+                        SignalServiceDataMessage.newBuilder()
+                                .withBody(message)
+                                .build());
+            }
         }
 
-        execute(map, listener);
     }
 
-    private void execute (HashMap<String, Object> map,
-                          @Nullable SignalExecutorTask.TaskResult taskResult) {
-        Main main = new Main(mContext);
-        new SignalExecutorTask(map, main, taskResult)
-                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    private class HavenSignalProtocolStore implements SignalProtocolStore {
+
+        @Override
+        public IdentityKeyPair getIdentityKeyPair() {
+            return null;
+        }
+
+        @Override
+        public int getLocalRegistrationId() {
+            return 0;
+        }
+
+        @Override
+        public boolean saveIdentity(SignalProtocolAddress address, IdentityKey identityKey) {
+            return false;
+        }
+
+        @Override
+        public boolean isTrustedIdentity(SignalProtocolAddress address, IdentityKey identityKey, Direction direction) {
+            return false;
+        }
+
+        @Override
+        public PreKeyRecord loadPreKey(int preKeyId) throws InvalidKeyIdException {
+            return null;
+        }
+
+        @Override
+        public void storePreKey(int preKeyId, PreKeyRecord record) {
+
+        }
+
+        @Override
+        public boolean containsPreKey(int preKeyId) {
+            return false;
+        }
+
+        @Override
+        public void removePreKey(int preKeyId) {
+
+        }
+
+        @Override
+        public SessionRecord loadSession(SignalProtocolAddress address) {
+            return null;
+        }
+
+        @Override
+        public List<Integer> getSubDeviceSessions(String name) {
+            return null;
+        }
+
+        @Override
+        public void storeSession(SignalProtocolAddress address, SessionRecord record) {
+
+        }
+
+        @Override
+        public boolean containsSession(SignalProtocolAddress address) {
+            return false;
+        }
+
+        @Override
+        public void deleteSession(SignalProtocolAddress address) {
+
+        }
+
+        @Override
+        public void deleteAllSessions(String name) {
+
+        }
+
+        @Override
+        public SignedPreKeyRecord loadSignedPreKey(int signedPreKeyId) throws InvalidKeyIdException {
+            return null;
+        }
+
+        @Override
+        public List<SignedPreKeyRecord> loadSignedPreKeys() {
+            return null;
+        }
+
+        @Override
+        public void storeSignedPreKey(int signedPreKeyId, SignedPreKeyRecord record) {
+
+        }
+
+        @Override
+        public boolean containsSignedPreKey(int signedPreKeyId) {
+            return false;
+        }
+
+        @Override
+        public void removeSignedPreKey(int signedPreKeyId) {
+
+        }
     }
+
 }
